0dc3f9df6267642714e38afc5e5f926ed4098eb2
Lowens1996 <32193115+Lowens1996@users.noreply.github.com>
2017-11-01 12:43:46 +0000

    Add files via upload

--- Lab4:Barrier/barrier.cpp
+++ Lab4:Barrier/barrier.cpp
@@ -2,12 +2,12 @@
 #include <iostream>
 #include <thread>
 #include <vector>
-/*Author:Lee Owens Last Edit Date: 24/10/2017*/
+/*Author:Lee Owens Last Edit Date: 01/11/2017*/
 
 int count= 0; //keep count of threads 
 int numOfThreads = 5; //thread pool size (used to compare against count) 
 
-void Task(std::shared_ptr<Semaphore> mutex, std::shared_ptr<Semaphore> barrier1, std::shared_ptr<Semaphore> barrier2, int count)
+void Task( std::shared_ptr<Semaphore> mutex, std::shared_ptr<Semaphore> barrier1, std::shared_ptr<Semaphore> barrier2)
 {
 
   std::cout <<"thread is entering ";
@@ -40,27 +40,37 @@ void Task(std::shared_ptr<Semaphore> mutex, std::shared_ptr<Semaphore> barrier1,
 }//task
 
 int main(void){
-  int index =0; //loop index
-  std::thread Thread;
-  std::vector<std::thread> threadPool;
-  std::shared_ptr<Semaphore> barrier1( new Semaphore (0));
-  std::shared_ptr<Semaphore> barrier2( new Semaphore (0));
+  int index =0; //loop index 
+  std::shared_ptr<Semaphore> barrier1( new Semaphore);
+  std::shared_ptr<Semaphore> barrier2( new Semaphore);
   std::shared_ptr<Semaphore> mutex( new Semaphore (1));
+  std::vector<std::thread> threadPool; //thread vector
+  
+  for(index=0; index<=numOfThreads; index++)
+    {
+       std::thread Thread;
+       Thread=std::thread(Task,mutex,barrier1,barrier2);
+    }
+  for(index=0; index<=numOfThreads; index++)
+    {
+      Thread.join();
+    }
  
-  //create thread vector  
+  /*create thread vector  
   for(int index=0; index <= numOfThreads; index++)
     {
+      std::thread Thread;
       threadPool.push_back(Thread);
     }
   //send threads to barrier
   for(int index=0; index <= threadPool.size(); index++)
     {
-      threadPool[index]=std::thread(mutex,barrier1,barrier2,count);
+      threadPool[index]=std::thread(Task,mutex,barrier1,barrier2);
     }
   //join threads back to main
   for(int index=0; index <= threadPool.size(); index++)
     {
       threadPool[index].join();
-    }
+      }*/
   return 0;
 }
